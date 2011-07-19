ab655b95ed3d13360fdc23db593e4bcfb830e501
Stefano B <stefano.ballabeni+github@gmail.com>
2011-07-19 17:17:30 -0400

    can pass style via hash

--- js/le-script.js
+++ js/le-script.js
@@ -3,3 +3,10 @@ $(function () {
     $('#le-style').html($(this).val());
   });
 });
+
+// #le-box{-webkit-transform: rotate(-90deg);-moz-transform: rotate(-90deg);transform: rotate(-90deg);}#main{overflow:hidden;}
+if (window.location.hash.match(/#s=/) !== null) {
+  append=window.location.hash.split('#s=')[1];
+  $('#le-input').val($('#le-input').val() + '\n' + append);
+  $('#le-style').html($('#le-input').val().replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+}
